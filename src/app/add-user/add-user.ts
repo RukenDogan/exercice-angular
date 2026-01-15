d94ab0e1e46f36246d4632583b656d774a4484a2
rukenDogan <ruken.dogan.rkn@gmail.com>
2026-01-15 17:30:37 +0100

    add

--- src/app/add-user/add-user.ts
+++ src/app/add-user/add-user.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-user',
-  imports: [],
-  templateUrl: './add-user.html',
-  styleUrl: './add-user.scss',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './add-user.component.html'
 })
-export class AddUser {
+export class AddUserComponent {
+  @Output() userAdded = new EventEmitter<any>();
+  @Output() close = new EventEmitter<void>();
 
+  newUser = {
+    nom: '',
+    prenom: '',
+    email: '',
+    tel: '',
+    adresse: '',
+    birthdate: ''
+  };
+
+  submit() {
+    this.userAdded.emit(this.newUser);
+    this.close.emit();
+    this.reset();
+  }
+
+  reset() {
+    this.newUser = { nom: '', prenom: '', email: '', tel: '', adresse: '', birthdate: '' };
+  }
 }
--- src/app/users/users.ts
+++ src/app/users/users.ts
@@ -1,36 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { RouterLink } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { UserService } from '../services/user.service';
+import { AddUserComponent } from '../add-user/add-user';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule, AddUserComponent],
   templateUrl: './users.html',
   styleUrls: ['./users.scss']
 })
 export class Users implements OnInit {
-
+  private users: any[] = [];
   users$!: Observable<any[]>;  // observable exposé au template
+  showModal = signal(false);
 
-  constructor(private http: HttpClient, private userService: UserService) { }
+  constructor(private http: HttpClient, private userService: UserService) {}
 
   ngOnInit(): void {
-    this.users$ = this.http.get<any[]>('/assets/data/user.json').pipe(
+    this.http.get<any[]>('/assets/data/user.json').pipe(
       map(users => users.map(u => ({
         ...u,
-        age: this.userService.calculerAge(u.birthdate) // ajoute directement l'âge
+        age: this.userService.calculerAge(u.birthdate)
       })))
-    );
+    ).subscribe(data => {
+      this.users = data;
+      this.updateObservable();
+    });
+  }
+
+  private updateObservable() {
+    this.users$ = of(this.users);
   }
 
   supprimer(id: number) {
-    this.users$ = this.users$.pipe(
-      map(users => users.filter(u => u.id !== id))
-    );
+    this.users = this.users.filter(u => u.id !== id);
+    this.updateObservable();
+  }
+
+  openModal() {
+    this.showModal.set(true);
+  }
+
+  closeModal() {
+    this.showModal.set(false);
+  }
+
+  ajouterUtilisateur(user: any) {
+    const nextId = this.users.length ? Math.max(...this.users.map(u => u.id)) + 1 : 1;
+    const userToAdd = {
+      ...user,
+      id: nextId,
+      age: this.userService.calculerAge(user.birthdate)
+    };
+    this.users.push(userToAdd);
+    this.updateObservable();
   }
 }
